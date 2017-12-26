6692cfa95742688d79dc2b91f3b8085138f92513
William Chong <williamc@tribalworldwide.ca>
2017-12-26 12:41:38 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var path = require('path');
+var merge = require('merge-stream');
+var del = require('del');
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var rename = require('gulp-rename');
+
+//convert sass to css
+gulp.task('sass', function() {
+  gulp.src('novon/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('novon/css/'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('novon/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
+gulp.task('deploy', ['sass']);
---
+++ novon/js/script.js
@@ -0,0 +1,68 @@
+$(function(){
+
+	// var mySwiper = new Swiper('.swiper-container', {
+	//     loop: true,
+	//     initialSlide: 2,
+	//     centeredSlides: true,
+	//     spaceBetween: 30,
+	//     pagination: {
+	//         el: '.swiper-pagination',
+	//         type: 'bullets',
+	//     },
+	//     navigation: {
+	//         nextEl: '.swiper-button-next',
+	//         prevEl: '.swiper-button-prev',
+	//     }
+	// });
+
+	$('.swiper-wrapper').slick({
+	  centerMode: true,
+	  centerPadding: '25%',
+	  dots: true,
+	  slidesToShow: 1
+	  // responsive: [
+	  //   {
+	  //     breakpoint: 768,
+	  //     settings: {
+	  //       arrows: false,
+	  //       centerMode: true,
+	  //       centerPadding: '40px',
+	  //       slidesToShow: 3
+	  //     }
+	  //   },
+	  //   {
+	  //     breakpoint: 480,
+	  //     settings: {
+	  //       arrows: false,
+	  //       centerMode: true,
+	  //       centerPadding: '40px',
+	  //       slidesToShow: 1
+	  //     }
+	  //   }
+	  // ]
+	});
+
+
+	var controller = new ScrollMagic.Controller(),
+	        tween1a = TweenMax.from($('#earrings-top'), 1, {y: "-60", opacity: 0}),
+	        tween1b = TweenMax.from($('#earrings-bottom'), 1, {y: "+60", opacity: 0}),
+	        // tween1c = TweenMax.to($('#earrings-bottom'), 1, {opacity: 0, delay: 1}),
+	        // tween1d = TweenMax.from($('#earrings-change'), 1, {y: "+60", opacity: 0}),
+	        scene = new ScrollMagic.Scene({triggerElement: "#modular", duration: "45%"});
+
+	scene.setTween([tween1a, tween1b]).addTo(controller);
+
+	var controller2 = new ScrollMagic.Controller(),
+	        tween1c = TweenMax.to($('#earrings-bottom'), 1, {opacity: 0, delay: 0.5}),
+	        tween1d = TweenMax.from($('#earrings-change'), 1, {y: "+60", opacity: 0, delay: 1}),
+	        scene2 = new ScrollMagic.Scene({triggerElement: "#earrings", duration: "40%"});
+
+	scene2.setTween([tween1c, tween1d]).addTo(controller2);
+
+	var controller3 = new ScrollMagic.Controller(),
+		tween3 = TweenMax.staggerFrom(".steps ol li", 1, {opacity:0, delay:0.5}, 0.5),
+		scene3 = new ScrollMagic.Scene({triggerElement: "#modular", duration: "50%"});
+
+		scene3.setTween([tween3]).addTo(controller3);
+
+});
